4c135207d038eb969266a5b16fc52d63f402a45e
rewardupbeat027 <rewardupbeat027@gmail.com>
2025-02-05 23:58:58 +0600

    7

--- vpn_project/deploy.sh
+++ vpn_project/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Путь до директории вашего проекта
-PROJECT_DIR="/root/New-/vpn-project"
+PROJECT_DIR="/root/pythonProject5/vpn_project"
 
 # Переходим в директорию проекта
 cd $PROJECT_DIR
